5caf68142a6fa0011ad6d80bb79afceb81bf98ef
Dinesh Singh <diineshsingh@gmail.com>
2019-10-15 02:12:01 +0530

    html content moved into app.component.html and scss path defines into app component

--- beginners-code-branch/src/app/app.component.ts
+++ beginners-code-branch/src/app/app.component.ts
@@ -1,6 +1,8 @@
 // importing core module /package for component.
 // The line below is saying we want to import the Component code from the module @angular/core.
 import { Component } from "@angular/core";
+import { Template } from '@angular/compiler/src/render3/r3_ast';
+import { templateJitUrl } from '@angular/compiler';
 
 // A decorator adds more behavior to our class from outside the class. It must be declared immediately before the class. [Components are the most basic UI building block of an Angular app.] * Decorators are a typeScript feature.
 @Component({
@@ -8,8 +10,11 @@ import { Component } from "@angular/core";
     selector: 'app-root',
 
     // template-  The content we want to load inside our selector. it's a blueprint for creating objects with specific functions and properties already attached to it.
-    template: '<h1>Welcome to Angular World...</h1>'
+    templateUrl: "./app.component.html",
+    styleUrls: ["./app.component.scss"]
 })
 
 //class:- it’s a blueprint for creating objects with specific functions and properties already attached to it.
-export class HelloAngular { }
+export class HelloAngular {
+    title = "Sending Title - Using interpolation to print properties.";
+}
