8148361c06629362e738e04b51c545a53e0e90d8
Dinesh Singh <diineshsingh@gmail.com>
2019-10-19 01:38:46 +0530

    ngFor and ngIf

--- beginners-code-branch/src/app/app.component.ts
+++ beginners-code-branch/src/app/app.component.ts
@@ -1,8 +1,6 @@
 // importing core module /package for component.
 // The line below is saying we want to import the Component code from the module @angular/core.
 import { Component } from "@angular/core";
-import { Template } from '@angular/compiler/src/render3/r3_ast';
-import { templateJitUrl } from '@angular/compiler';
 
 // A decorator adds more behavior to our class from outside the class. It must be declared immediately before the class. [Components are the most basic UI building block of an Angular app.] * Decorators are a typeScript feature.
 @Component({
@@ -17,4 +15,22 @@ import { templateJitUrl } from '@angular/compiler';
 //class:- it’s a blueprint for creating objects with specific functions and properties already attached to it.
 export class HelloAngular {
     title = "Sending Title - Using interpolation to print properties.";
+    mobParts = [{
+        'id': 1001,
+        'name': "Screen 5.5",
+        'description': "5.5 Screen for Moto g",
+        'inStock': 5
+    },
+    {
+        'id': 1002,
+        'name': "Screen 5",
+        'description': "5 Screen for Samsung",
+        'inStock': 8
+    },
+    {
+        'id': 1003,
+        'name': "key panel",
+        'description': "Key panel for Nokia",
+        'inStock': 0
+    }];
 }
